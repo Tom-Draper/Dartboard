9ecc51638159c0d14211ad668902fb52bf8b81d8
Tom Draper <tomjdraper1@gmail.com>
2020-06-11 12:29:55 +0100

    Apply gaussian

--- gaussian.py
+++ gaussian.py
@@ -10,7 +10,6 @@ class Gaussian():
         x, y = np.meshgrid(np.linspace(-1,1,size), np.linspace(-1,1,size))
         d = np.sqrt(x*x + y*y)
         self.gaussian = np.exp(-((d-mu)**2 / (2.0 * sigma**2)))
-        print(len(self.gaussian))
 
     def printGaussian(self):
         print(self.gaussian)
@@ -22,15 +21,16 @@ class Gaussian():
         
         # If gaussian even length, take top left of the central four as mid point
         if len(self.gaussian) % 2 == 0:
-            y_start = point[0] - (len(gaussian)/2 - 1)
-            x_start = point[1] - (len(gaussian[0])/2 - 1)
+            y_start = int(point[0] - len(self.gaussian)/2)
+            x_start = int(point[1] - len(self.gaussian[0])/2)
             
             p = 0
-            for y in range(y_start, y_start + len(gaussian)):
-                for x in range(x_start, x_start + len(gaussian[0])):
+            for y in range(y_start, y_start + len(self.gaussian)):
+                for x in range(x_start, x_start + len(self.gaussian[0])):
                     if y < dartboard.shape[0] and x < dartboard.shape[1] and y > 0 and x > 0:
-                        p += self.gaussian[y][x] * dartboard[y][x]
+                        p += self.gaussian[y - y_start][x - x_start] * dartboard[y][x]
             print(p)
+            
             return p
         
 
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import numpy as np
 from create_board import CreateDartboard
 from gaussian import Gaussian
-from printer import Printer
 
 
-create = CreateDartboard('dartboard_img/dartboard.png')
-create.run()
+board = CreateDartboard('dartboard_img/dartboard.png')
+board.run()
+board.printBoardSection((600,600), 100)
+
 dartboard = np.load('dartboard.npy')
-p = Printer()
-p.printBoardSection(dartboard, 600,600)
+
 gaussian = Gaussian()
-gaussian.calculateGaussian(1, 0, 10)
+gaussian.calculateGaussian(1, 0, 100)
+gaussian.printGaussian()
 gaussian.applyGaussian(dartboard, (600,600))
